bf8309341d516f664b3f5333677fd7bbaee04557
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2024-10-11 20:47:27 +0000

    finished editing classes

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,4 +1,6 @@
 class DeliveriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     # if current_user != nil
     matching_deliveries = Delivery.where({ :user_id => current_user.id })
